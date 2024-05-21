2d015ebead10a663d2e5b77a46f3992e7cadf79f
Filip Manjencic <fmanjencic@gmail.com>
2024-05-21 10:54:02 -0400

    Python In operator

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
+
+#IN Operator
+names = ['jon', 'kalob', 'Filip']
+print ('jon' in names) #returns as true
+if 'kalob' in names:
+    print('Kalob is in the list of names')
+
+key = 'name'
+person = {
+    'name': 'doug'
+}
+if key in person:
+    print('Can use IN operator to check for an object key')   
+
+fruit = 'apple'
+myset = {"apple", "banana", "cherry"}
+if fruit in myset:
+    print('Yes In op works for Python sets too.')     
+
 #Python Playground for review
 for i in range(10):
     print(i)
